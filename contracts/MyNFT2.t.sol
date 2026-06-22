4af8724e7a1704313b2f9573ada3de8b4f155348
ueki ryugo <b233141@hiroshima-u.ac.jp>
2026-06-22 20:59:43 +0900

    テストファイルを修正

--- contracts/MyNFT2.t.sol
+++ contracts/MyNFT2.t.sol
@@ -1,231 +1,246 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "forge-std/Test.sol";
-import "../src/MyNFT2.sol";
+import { MyNFT2 } from "./MyNFT2.sol";
+import { Test } from "forge-std/Test.sol";
 
 contract MyNFT2Test is Test {
     MyNFT2 nft;
 
-    address owner = address(0x1);
-    address user1 = address(0x2);
-    address user2 = address(0x3);
-    address operator = address(0x4);
+    address owner = address(this);
+    address user1 = address(0x1);
+    address user2 = address(0x2);
+    address operator = address(0x3);
+
+    uint256 tokenId = 1;
 
     function setUp() public {
-        nft = new MyNFT2("MyNFT2", "MNFT2");
+        nft = new MyNFT2("MyNFT", "MNFT");
     }
 
-    // name と symbol が正しく設定されているか
-    function testNameAndSymbol() public view {
-        assertEq(nft.name(), "MyNFT2");
-        assertEq(nft.symbol(), "MNFT2");
+    /// デプロイ時の正常系
+
+    function testNFTNameAndSymbol() public view {
+        assertEq(nft.name(), "MyNFT");
+        assertEq(nft.symbol(), "MNFT");
     }
 
-    // mint すると所有者と残高が正しく変わるか
-    function testMint() public {
-        nft.mint(owner, 1);
-
-        assertEq(nft.ownerOf(1), owner);
-        assertEq(nft.balanceOf(owner), 1);
+    function testInitialBalanceIsZero() public view {
+        assertEq(nft.balanceOf(owner), 0);
+        assertEq(nft.balanceOf(user1), 0);
     }
 
-    // mint 時に Transfer イベントが発行されるか
-    function testMintEmitsTransferEvent() public {
-        vm.expectEmit(true, true, true, true);
-        emit IERC721.Transfer(address(0), owner, 1);
+    /// mintの正常系
 
-        nft.mint(owner, 1);
+    function testMintSuccess() public {
+        nft.mint(user1, tokenId);
+
+        assertEq(nft.ownerOf(tokenId), user1);
+        assertEq(nft.balanceOf(user1), 1);
     }
 
-    // 0アドレスには mint できない
-    function testCannotMintToZeroAddress() public {
+    /// mintのエラーケース
+
+    function testMintFailsToZeroAddress() public {
         vm.expectRevert("NFT: mint to zero address");
-        nft.mint(address(0), 1);
+        nft.mint(address(0), tokenId);
     }
 
-    // 同じ tokenId は二重に mint できない
-    function testCannotMintSameTokenIdTwice() public {
-        nft.mint(owner, 1);
+    function testMintFailsWhenTokenIdAlreadyExists() public {
+        nft.mint(user1, tokenId);
 
         vm.expectRevert("NFT: token already minted");
-        nft.mint(user1, 1);
+        nft.mint(user2, tokenId);
     }
 
-    // 存在しない tokenId の ownerOf は失敗する
-    function testOwnerOfNonexistentTokenReverts() public {
-        vm.expectRevert("NFT: owner query for nonexistent token");
-        nft.ownerOf(999);
-    }
+    /// balanceOf・ownerOfのエラーケース
 
-    // balanceOf に 0アドレスを指定すると失敗する
-    function testBalanceOfZeroAddressReverts() public {
+    function testBalanceOfFailsForZeroAddress() public {
         vm.expectRevert("NFT: balance query for zero address");
         nft.balanceOf(address(0));
     }
 
-    // 所有者は transferFrom できる
-    function testOwnerCanTransfer() public {
-        nft.mint(owner, 1);
-
-        vm.prank(owner);
-        nft.transferFrom(owner, user1, 1);
-
-        assertEq(nft.ownerOf(1), user1);
-        assertEq(nft.balanceOf(owner), 0);
-        assertEq(nft.balanceOf(user1), 1);
+    function testOwnerOfFailsForNonexistentToken() public {
+        vm.expectRevert("NFT: owner query for nonexistent token");
+        nft.ownerOf(tokenId);
     }
 
-    // 所有者でない人は transferFrom できない
-    function testNonOwnerCannotTransfer() public {
-        nft.mint(owner, 1);
+    /// approveの正常系
 
-        vm.prank(user1);
-        vm.expectRevert("NFT: caller is not owner nor approved");
-        nft.transferFrom(owner, user2, 1);
+    function testApproveSuccess() public {
+        nft.mint(owner, tokenId);
+
+        nft.approve(user1, tokenId);
+
+        assertEq(nft.getApproved(tokenId), user1);
     }
 
-    // from が現在の所有者でない場合は transferFrom できない
-    function testCannotTransferFromIncorrectOwner() public {
-        nft.mint(owner, 1);
-
-        vm.prank(owner);
-        vm.expectRevert("NFT: transfer from incorrect owner");
-        nft.transferFrom(user1, user2, 1);
-    }
-
-    // 0アドレスには transfer できない
-    function testCannotTransferToZeroAddress() public {
-        nft.mint(owner, 1);
-
-        vm.prank(owner);
-        vm.expectRevert("NFT: transfer to zero address");
-        nft.transferFrom(owner, address(0), 1);
-    }
-
-    // approve された人は transferFrom できる
     function testApprovedAddressCanTransfer() public {
-        nft.mint(owner, 1);
-
-        vm.prank(owner);
-        nft.approve(user1, 1);
-
-        assertEq(nft.getApproved(1), user1);
+        nft.mint(owner, tokenId);
+        nft.approve(user1, tokenId);
 
         vm.prank(user1);
-        nft.transferFrom(owner, user2, 1);
+        nft.transferFrom(owner, user2, tokenId);
 
-        assertEq(nft.ownerOf(1), user2);
+        assertEq(nft.ownerOf(tokenId), user2);
+        assertEq(nft.balanceOf(owner), 0);
+        assertEq(nft.balanceOf(user2), 1);
+        assertEq(nft.getApproved(tokenId), address(0));
     }
 
-    // approve すると Approval イベントが発行される
-    function testApproveEmitsApprovalEvent() public {
-        nft.mint(owner, 1);
+    /// approveのエラーケース
 
-        vm.prank(owner);
-        vm.expectEmit(true, true, true, true);
-        emit IERC721.Approval(owner, user1, 1);
-
-        nft.approve(user1, 1);
-    }
-
-    // 所有者本人には approve できない
-    function testCannotApproveCurrentOwner() public {
-        nft.mint(owner, 1);
-
-        vm.prank(owner);
-        vm.expectRevert("NFT: approval to current owner");
-        nft.approve(owner, 1);
-    }
-
-    // 所有者でも operator でもない人は approve できない
-    function testNonOwnerCannotApprove() public {
-        nft.mint(owner, 1);
+    function testApproveFailsWhenCallerIsNotOwner() public {
+        nft.mint(owner, tokenId);
 
         vm.prank(user1);
         vm.expectRevert("NFT: approve caller is not owner nor approved for all");
-        nft.approve(user2, 1);
+        nft.approve(user2, tokenId);
     }
 
-    // 存在しない tokenId の getApproved は失敗する
-    function testGetApprovedForNonexistentTokenReverts() public {
+    function testApproveFailsToCurrentOwner() public {
+        nft.mint(owner, tokenId);
+
+        vm.expectRevert("NFT: approval to current owner");
+        nft.approve(owner, tokenId);
+    }
+
+    function testGetApprovedFailsForNonexistentToken() public {
         vm.expectRevert("NFT: approved query for nonexistent token");
-        nft.getApproved(999);
+        nft.getApproved(tokenId);
     }
 
-    // setApprovalForAll で operator に全体承認できる
-    function testSetApprovalForAll() public {
-        vm.prank(owner);
+    /// setApprovalForAllの正常系
+
+    function testSetApprovalForAllSuccess() public {
         nft.setApprovalForAll(operator, true);
 
         assertTrue(nft.isApprovedForAll(owner, operator));
     }
 
-    // operator は所有者のNFTを transferFrom できる
-    function testOperatorCanTransfer() public {
-        nft.mint(owner, 1);
+    function testSetApprovalForAllCanBeCancelled() public {
+        nft.setApprovalForAll(operator, true);
+        nft.setApprovalForAll(operator, false);
 
-        vm.prank(owner);
+        assertFalse(nft.isApprovedForAll(owner, operator));
+    }
+
+    function testOperatorCanTransfer() public {
+        nft.mint(owner, tokenId);
         nft.setApprovalForAll(operator, true);
 
         vm.prank(operator);
-        nft.transferFrom(owner, user1, 1);
+        nft.transferFrom(owner, user1, tokenId);
 
-        assertEq(nft.ownerOf(1), user1);
+        assertEq(nft.ownerOf(tokenId), user1);
+        assertEq(nft.balanceOf(owner), 0);
+        assertEq(nft.balanceOf(user1), 1);
     }
 
-    // 自分自身を operator にはできない
-    function testCannotApproveCallerAsOperator() public {
-        vm.prank(owner);
+    function testOperatorCanApproveAnotherAddress() public {
+        nft.mint(owner, tokenId);
+        nft.setApprovalForAll(operator, true);
+
+        vm.prank(operator);
+        nft.approve(user1, tokenId);
+
+        assertEq(nft.getApproved(tokenId), user1);
+    }
+
+    /// setApprovalForAllのエラーケース
+
+    function testSetApprovalForAllFailsToCaller() public {
         vm.expectRevert("NFT: approve to caller");
         nft.setApprovalForAll(owner, true);
     }
 
-    // transfer 後は approve 情報が削除される
-    function testApprovalIsClearedAfterTransfer() public {
-        nft.mint(owner, 1);
+    /// transferFromの正常系
 
-        vm.prank(owner);
-        nft.approve(user1, 1);
+    function testOwnerCanTransfer() public {
+        nft.mint(owner, tokenId);
 
-        vm.prank(owner);
-        nft.transferFrom(owner, user2, 1);
+        nft.transferFrom(owner, user1, tokenId);
 
-        assertEq(nft.getApproved(1), address(0));
+        assertEq(nft.ownerOf(tokenId), user1);
+        assertEq(nft.balanceOf(owner), 0);
+        assertEq(nft.balanceOf(user1), 1);
     }
 
-    // 所有者は burn できる
-    function testOwnerCanBurn() public {
-        nft.mint(owner, 1);
+    /// transferFromのエラーケース
 
-        vm.prank(owner);
-        nft.burn(1);
+    function testTransferFailsWhenCallerHasNoPermission() public {
+        nft.mint(owner, tokenId);
+
+        vm.prank(user1);
+        vm.expectRevert("NFT: caller is not owner nor approved");
+        nft.transferFrom(owner, user2, tokenId);
+    }
+
+    function testTransferFailsFromIncorrectOwner() public {
+        nft.mint(owner, tokenId);
+
+        vm.expectRevert("NFT: transfer from incorrect owner");
+        nft.transferFrom(user1, user2, tokenId);
+    }
+
+    function testTransferFailsToZeroAddress() public {
+        nft.mint(owner, tokenId);
+
+        vm.expectRevert("NFT: transfer to zero address");
+        nft.transferFrom(owner, address(0), tokenId);
+    }
+
+    /// burnの正常系
+
+    function testOwnerCanBurn() public {
+        nft.mint(owner, tokenId);
+
+        nft.burn(tokenId);
 
         assertEq(nft.balanceOf(owner), 0);
 
         vm.expectRevert("NFT: owner query for nonexistent token");
-        nft.ownerOf(1);
+        nft.ownerOf(tokenId);
     }
 
-    // approve された人は burn できる
     function testApprovedAddressCanBurn() public {
-        nft.mint(owner, 1);
-
-        vm.prank(owner);
-        nft.approve(user1, 1);
+        nft.mint(owner, tokenId);
+        nft.approve(user1, tokenId);
 
         vm.prank(user1);
-        nft.burn(1);
+        nft.burn(tokenId);
 
         assertEq(nft.balanceOf(owner), 0);
+
+        vm.expectRevert("NFT: owner query for nonexistent token");
+        nft.ownerOf(tokenId);
     }
 
-    // 権限がない人は burn できない
-    function testNonOwnerCannotBurn() public {
-        nft.mint(owner, 1);
+    function testOperatorCanBurn() public {
+        nft.mint(owner, tokenId);
+        nft.setApprovalForAll(operator, true);
+
+        vm.prank(operator);
+        nft.burn(tokenId);
+
+        assertEq(nft.balanceOf(owner), 0);
+
+        vm.expectRevert("NFT: owner query for nonexistent token");
+        nft.ownerOf(tokenId);
+    }
+
+    /// burnのエラーケース
+
+    function testBurnFailsWhenCallerHasNoPermission() public {
+        nft.mint(owner, tokenId);
 
         vm.prank(user1);
         vm.expectRevert("NFT: caller is not owner nor approved");
-        nft.burn(1);
+        nft.burn(tokenId);
+    }
+
+    function testBurnFailsForNonexistentToken() public {
+        vm.expectRevert("NFT: owner query for nonexistent token");
+        nft.burn(tokenId);
     }
 }
